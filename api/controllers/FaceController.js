d466a8529e3a09c81a354a6025a780e80788f980
Zios27012 <nathan.m.g.gallet@gmail.com>
2016-08-21 15:15:41 +0200

    [algo elo] beginning of elo ranking

--- api/controllers/FaceController.js
+++ api/controllers/FaceController.js
@@ -5,6 +5,8 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 //TODO : check faces array
+
+
 //TODO : check id
 module.exports = {
     upVote: function(req, res) {
@@ -17,12 +19,17 @@ module.exports = {
                 ranking_self     = faces[0].rank;
                 ranking_opponent = faces[1].rank;
 
-                res.ok();
+                updateRating(ranking_self, ranking_opponent, res);
             })
-        // var constant = 15;
-        // quote_one = Math.pow(10)
-
     }
 
 };
 
+function updateRating(ranking_self, ranking_opponent, res) {
+
+    var constant = 15;
+    quote_self = Math.pow(10, ranking_self/400);
+    quote_opponent = Math.pow(10, ranking_opponent/400);
+    ranking_self += parseInt(qu)
+    res.ok();
+}
